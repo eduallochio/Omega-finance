654f0bd271369f67f6881ed48a81852da7d0bd96
Eduardo Allochio <eduallochio2@outlook.com>
2025-01-20 14:19:17 -0300

    retirada do código extra do dashboard

--- Frontend/src/components/dashboard/Dashboard.jsx
+++ Frontend/src/components/dashboard/Dashboard.jsx
@@ -1,22 +1,32 @@
-// src/components/dashboard/Dashboard.jsx
-import Header from "../Header";
-import Tabs from "../Tabs";
+
+import Header from "../Header"; 
+import Tabs from "../Tabs"; 
 import "./Dashboard.css";
 import PropTypes from 'prop-types';
 
 const Dashboard = ({ children }) => {
   return (
     <div className="dashboard">
+      {/* Cabeçalho */}
       <Header />
-      <Tabs />
-      <main className="dashboard-content">{children}</main>
+      
+      {/* Conteúdo do Dashboard */}
+      <div className="dashboard-content">
+        {/* Área Principal */}
+        <main className="dashboard-main">
+          {/* Tabs para navegação adicional */}
+          <Tabs />
+          
+          {/* Conteúdo principal */}
+          {children}
+        </main>
+      </div>
     </div>
   );
 };
-
-
 Dashboard.propTypes = {
   children: PropTypes.node
 };
 
 export default Dashboard;
+
